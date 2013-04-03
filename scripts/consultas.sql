b3c2d9913f7c795ce76d322af9e1f6c43da5f022
David Y. Gonzalez <dvdyzag@gmail.com>
2013-04-03 09:15:43 -0600

    Entrega Fase I

---
+++ scripts/consultas.sql
@@ -0,0 +1,38 @@
+﻿--consiga el titulo, precio y cantidad de ventas de todos los productos a un precio que ocila entre 22 y 25, que se hayan vendido mas de 20.
+select title, price, sales 
+from producto 
+where price > 22 and price < 25 and sales > 20 
+order by price ASC;
+
+--consiga los nombres de los clientes que su primer nombre empiece con las letras FU
+select firstname, lastname from cliente where firstname like 'FU%';
+
+--una lista de clientes que tienen mas de 30 ordenes
+select cl.firstname, ordenes.NumOrden
+from cliente cl
+inner join (
+	select customerid, count(orderid) as NumOrden
+	from orden
+	group by customerid
+	having count(orderid)>30) ordenes
+on (cl.customerid=ordenes.customerid)
+
+--muestre el numero de clientes por pais
+select country, conteo 
+from pais 
+inner join (
+	select country_id, count(firstname) as conteo
+	from cliente 
+	group by country_id) clientes 
+on (pais.country_id = clientes.country_id);
+
+--Lista de vendedores y cantidad de ventas
+select vendedor.vendor_name, NumVentas
+from vendedor
+inner join (
+	select vendor_id, count(distinct orderid) as NumVentas
+	from orden
+	group by vendor_id
+) vendedores
+on (vendedor.vendor_id = vendedores.vendor_id)
+order by NumVentas desc
--- scripts/ddl-tienda-postgresql.sql
+++ scripts/ddl-tienda-postgresql.sql
@@ -8,23 +8,23 @@ CREATE TABLE pais (
 );
 CREATE TABLE cliente (
 	customerid INTEGER PRIMARY KEY, 
-	firstname VARCHAR(50),
+	firstname VARCHAR(50) NOT NULL,
 	lastname VARCHAR(50),
-	address1 VARCHAR(50),
+	address1 VARCHAR(50) NOT NULL,
 	zip INTEGER,
 	phone VARCHAR(25),
 	creditcard VARCHAR(50),
 	age SMALLINT,
 	income INTEGER,
-	gender CHAR(1),
+	gender CHAR(1) NOT NULL,
 	country_id INTEGER NOT NULL REFERENCES pais(country_id)
 );
 --REPLACE NUMBER WITH NUMERIC
 CREATE TABLE producto (
 	prod_id INTEGER PRIMARY KEY,
-	title VARCHAR(50),
+	title VARCHAR(50) NOT NULL,
 	actor VARCHAR(50),
-	price NUMERIC(12,2),
+	price NUMERIC(12,2) NOT NULL,
 	special SMALLINT,
 	quan_in_stock INTEGER,
 	sales INTEGER,
@@ -33,24 +33,24 @@ CREATE TABLE producto (
 );
 CREATE TABLE vendedor (
 	vendedor_id INTEGER PRIMARY KEY,
-	vendedor_name VARCHAR(50)
+	vendedor_name VARCHAR(50) NOT NULL
 );
 CREATE TABLE orden (
-	orderid INTEGER NOT NULL,
-	orderlineid INTEGER NOT NULL,
-	quantity SMALLINT,
-	orderdate DATE,
-	price NUMERIC(12,2),
+	orderid INTEGER PRIMARY KEY,
 	prod_id INTEGER NOT NULL REFERENCES producto(prod_id),
 	customerid INTEGER NOT NULL REFERENCES cliente(customerid),
-	vendedor_id INTEGER NOT NULL REFERENCES vendedor(vendedor_id),
-	PRIMARY KEY (orderid, orderlineid)
+	vendedor_id INTEGER NOT NULL REFERENCES vendedor(vendedor_id)
+);
+CREATE TABLE orden_detalle (
+	orderlineid INTEGER PRIMARY KEY,
+	quantity SMALLINT NOT NULL,
+	orderdate DATE NOT NULL,
+	price NUMERIC(12,2),
+	orderid INTEGER NOT NULL REFERENCES orden(orderid)
 );
 CREATE TABLE cliente_historial (
 	cli_hist_id SERIAL PRIMARY KEY,
 	customerid INTEGER NOT NULL REFERENCES cliente(customerid),
-	orderid INTEGER NOT NULL,
-	orderlineid INTEGER NOT NULL,
-	prod_id INTEGER REFERENCES producto(prod_id),
-	FOREIGN KEY(orderid, orderlineid) REFERENCES orden(orderid, orderlineid)
+	orderid INTEGER NOT NULL REFERENCES orden(orderid),
+	prod_id INTEGER REFERENCES producto(prod_id)
 );
